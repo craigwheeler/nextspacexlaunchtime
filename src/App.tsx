981e758c4902144c85454e9717402e4ee2966427
Craig Wheeler <craig@craigwheeler.net>
2021-02-06 19:52:40 -0500

    setup graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Rocket from './components/Rocket';
 import CountdownTimer from './components/CountdownTimer';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { RestLink } from 'apollo-link-rest';
+import gql from 'graphql-tag';
+
+const restLink = new RestLink({ uri: 'https://api.spacexdata.com/v4/' });
+
+const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache()
+});
+
+const query = gql`
+  query {
+    launch @rest(type: "Launches", path: "launches/next") {
+      rocket
+      date_utc
+    }
+  }
+`;
 
 const App = () => {
-  const rocketId = '5e9d0d95eda69974db09d1ed';
-  const launchDate = '2021-02-12T05:25:00.000Z';
+  const [launch, setLaunch] = useState({
+    rocket: '',
+    date_utc: ''
+  });
+
+  useEffect(() => {
+    client.query({ query }).then((response) => {
+      setLaunch(response.data.launch);
+    });
+  }, []);
+
+  if (!launch.date_utc) return <div>loading spinner...</div>;
 
   return (
     <LayoutContainer>
       <Header />
       <Content>
-        <CountdownTimer date={launchDate} />
-        <Rocket id={rocketId} />
+        <CountdownTimer date={launch.date_utc} />
+        <Rocket id={launch.rocket} />
       </Content>
       <Footer />
     </LayoutContainer>
